30ee75f2e604417672707590e17ec4b7e011f45d
Ahmet Çevrim <purplecevrim@gmail.com>
2025-04-27 18:37:17 +0300

    Refactor event registration routes for clarity and consistency

--- routes/routes.go
+++ routes/routes.go
@@ -11,9 +11,11 @@ func RegisterRoutes(server *gin.Engine) {
 
 	authenticated := server.Group("/")
 	authenticated.Use(middlewares.Authenticate)
-	authenticated.POST("/events", createEvent) // auth reqired
-	authenticated.PUT("/events/:id", updateEvent)
-	authenticated.DELETE("/events/:id", deleteEvent)
+	authenticated.POST("/events", createEvent)       // auth reqired
+	authenticated.PUT("/events/:id", updateEvent)    // auth reqired and just update by who create it
+	authenticated.DELETE("/events/:id", deleteEvent) // auth reqired and just update by who create it
+	authenticated.POST("/events/:id/register", registerForEvent)
+	authenticated.DELETE("/events/:id/register", cancelRegistration)
 
 	server.POST("/signup", signup)
 	server.POST("/login", login)
