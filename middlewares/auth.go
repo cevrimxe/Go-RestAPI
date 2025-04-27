501792db02a9f8a8f146257c5696e2ba4a23324c
Ahmet Çevrim <purplecevrim@gmail.com>
2025-04-27 17:13:36 +0300

    Add authentication middleware to verify JWT and extract user ID

---
+++ middlewares/auth.go
@@ -0,0 +1,28 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/cevrimxe/Go-RestAPI/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func Authenticate(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+
+	if token == "" {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "not authorized token empty"})
+		return
+	}
+
+	userId, err := utils.VerifyToken(token)
+
+	if err != nil {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "not authorized"})
+		return
+	}
+
+	context.Set("userId", userId)
+
+	context.Next()
+}
